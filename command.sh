2121ee082757dec27f71971dc5bf865f7e1f1ffa
ngrxmu <31520211154072@stu.xmu.edu.cn>
2023-06-19 16:03:12 +0800

    new

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python test_quant.py --model swin_tiny --dataset /bfs/ngr/lmb/imagenet --mode 0 --w_bit 8 --a_bit 8 --seed 444
+CUDA_VISIBLE_DEVICES=0 python test_quant.py --model swin_tiny --dataset PATH_TO_IMAGENET --mode 0 --w_bit 8 --a_bit 8 --seed 444
